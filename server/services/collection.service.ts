1673d048c723aa68ea8a439891e1cf9dd913694f
Nitsa Kanungo <kanungo.n@northeastern.edu>
2025-03-01 18:23:15 -0500

    collections schema, model; new collection and get collections service functions and routes

---
+++ server/services/collection.service.ts
@@ -0,0 +1,61 @@
+import CollectionModel from '../models/collections.model';
+import UserModel from '../models/users.model';
+import {
+  DatabaseCollection,
+  Collection,
+  CollectionResponse,
+  CollectionsResponse,
+  DatabaseUser,
+} from '../types/types';
+
+/**
+ * Saves a new collection to the database.
+ *
+ * @param {Collection} collection - the collection object to be saved.
+ * @returns {Promise<CollectionResponse>} - resolves with the saved collection object or an error message.
+ */
+export const saveCollection = async (collection: Collection): Promise<CollectionResponse> => {
+  try {
+    const userExists: DatabaseUser | null = await UserModel.findOne({ username: collection.username });
+
+    if (!userExists) {
+      throw new Error('User does not exist.');
+    }
+
+    const result: DatabaseCollection = await CollectionModel.create(collection);
+
+    if (!result) {
+      throw Error('Failed to create collection');
+    }
+
+    return result;
+  } catch (error) {
+    return { error: `Error occurred when saving collection: ${error}` };
+  }
+};
+
+/**
+ * Retrieves all collections of the provided user.
+ *
+ * @param username - the username of the user.
+ * @returns {Promise<CollectionsResponse>} - an array of collections or an empty array.
+ */
+export const getCollectionsByUser = async (username: string): Promise<CollectionsResponse> => {
+  try {
+    const userExists: DatabaseUser | null = await UserModel.findOne({ username: username });
+
+    if (!userExists) {
+      throw new Error('User does not exist.');
+    }
+
+    const collections = await CollectionModel.find({ username: username });
+
+    if (!collections) {
+      throw new Error('Collections not found for the provided user');
+    }
+
+    return collections;
+  } catch {
+    return [];
+  }
+};
