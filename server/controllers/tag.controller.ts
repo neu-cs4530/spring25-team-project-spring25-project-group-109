2a8f3cb6b759d1038e699b90314e6a09acb8b68d
Nitsa Kanungo <kanungo.n@northeastern.edu>
2025-03-21 14:08:48 -0400

    move all youtube things to their own files

--- server/controllers/tag.controller.ts
+++ server/controllers/tag.controller.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response, Router } from 'express';
 import {
-  fetchYoutubeVideos,
   getTagCountMap,
   getMostRecentQuestionTags,
 } from '../services/tag.service';
@@ -87,35 +86,12 @@ const tagController = () => {
     }
   };
 
-  /**
-   * Retrieves YouTube videos based on the tags from the most recent question asked by a given user.
-   *
-   * @param req The HTTP request object, containing the askedBy parameter in the URL.
-   * @param res The HTTP response object used to send back the list of YouTube videos.
-   *
-   * @returns A Promise that resolves to void.
-   */
-  const getYoutubeVideosRoute = async (req: Request, res: Response): Promise<void> => {
-    try {
-      const { askedBy } = req.params; // Get the user ID from the request parameters
-      const videos = await fetchYoutubeVideos(askedBy);
 
-      if ('error' in videos) {
-        res.status(500).send('No YouTube videos found for these tags.');
-        return;
-      }
-
-      res.json(videos); // Return the videos as JSON
-    } catch (err) {
-      res.status(500).send(`Error fetching YouTube videos: ${(err as Error).message}`);
-    }
-  };
 
   // Add appropriate HTTP verbs and their endpoints to the router.
   router.get('/getTagsWithQuestionNumber', getTagsWithQuestionNumber);
   router.get('/getTagByName/:name', getTagByName); // New endpoint to get tag by name
   router.get('/getMostRecentQuestionTags/:askedBy', getMostRecentQuestionTagsRoute); // Route to get tags by user
-  router.get('/getYoutubeVideos/:askedBy', getYoutubeVideosRoute); // New endpoint route to get YouTube videos by user
 
   return router;
 };
---
+++ server/controllers/youtubevideos.controller.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response, Router } from 'express';
+import { fetchYoutubeVideos } from '../services/youtubevideos.service';
+
+const youtubeController = () => {
+  const router: Router = express.Router();
+
+  /**
+   * Retrieves YouTube videos based on the tags from the most recent question asked by a given user.
+   *
+   * @param req The HTTP request object, containing the askedBy parameter in the URL.
+   * @param res The HTTP response object used to send back the list of YouTube videos.
+   *
+   * @returns A Promise that resolves to void.
+   */
+  const getYoutubeVideosRoute = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { askedBy } = req.params; // Get the user ID from the request parameters
+      const videos = await fetchYoutubeVideos(askedBy);
+
+      if ('error' in videos) {
+        res.status(500).send('No YouTube videos found for these tags.');
+        return;
+      }
+
+      res.json(videos); // Return the videos as JSON
+    } catch (err) {
+      res.status(500).send(`Error fetching YouTube videos: ${(err as Error).message}`);
+    }
+  };
+
+  router.get('/getYoutubeVideos/:askedBy', getYoutubeVideosRoute); 
+  return router;
+};
+
+export default youtubeController;
